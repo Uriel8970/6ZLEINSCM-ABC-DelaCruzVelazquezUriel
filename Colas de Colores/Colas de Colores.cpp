e5b2b86d184e04e155d2ea0d655a033c89f186c8
Uriel8970 <u.delacruz.v@edu.utc.mx>
2022-07-06 14:11:18 -0500

    Colas de Colores

---
+++ Colas de Colores/Colas de Colores.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+
+using namespace std;
+
+//estructura
+struct nodo{
+	string color; 
+	struct nodo *siguiente;
+};
+
+//estructura cola
+struct cola{
+	nodo *ultimo;
+	nodo *primero;
+};
+
+//encolar
+void encolar(struct cola &q, string c){
+	//lo que hay adentro de la cola
+	struct nodo *aux = new(struct nodo);
+	
+	aux ->color = c;
+	aux ->siguiente = NULL;
+	
+	if(q.ultimo == NULL){
+		q.ultimo = aux; //encolar el primer elemento
+	}else{
+		(q.primero)->siguiente = aux;
+	}
+	//el puntero siempre apunta al ultimo
+	q.primero = aux;
+}
+
+//desencolar
+string desencolar(struct cola &q){
+	string col;
+	struct nodo *aux;
+	
+	//aux apunte al inicio de la cola
+	aux = q.ultimo;
+	col = aux->color;
+	q.ultimo = (q.ultimo)->siguiente;
+	
+	//dale delete a aux
+	delete(aux);
+	
+	return col;
+}
+
+//mostrar colita
+void muestracola(struct cola q){
+	//nodo auxiliar
+	struct nodo *aux;
+	//siempre el nodo hasta delante
+	aux = q.ultimo;
+	//cola vacia
+	while(aux!=NULL){
+		cout<<aux->color<<" --> ";
+		aux = aux->siguiente;
+	}
+}
+
+//vaciar la cola, eliminar
+void vaciarcola(struct cola &q){
+	//auxiliar para eliminar cada elemento
+	struct nodo *aux;
+	//si esta vacia
+	while(q.ultimo != NULL){
+		aux = q.ultimo;
+		q.ultimo = aux->siguiente;
+		delete(aux);
+	}
+	q.ultimo = NULL;
+	q.primero = NULL;
+}
+
+void menu(){
+	cout<<"\n\t Colita de Colores\n";
+	cout<<"\t 1. Encolar\n";
+	cout<<"\t 2. Desencolar\n";
+	cout<<"\t 3. Mostrar\n";
+	cout<<"\t 4. Vaciar\n";
+	cout<<"\t 5. Salir\n";
+}
+
+int main(){
+	//primero declaramos la cola
+	struct cola q;
+	
+	//defino la cola con sus apuntadores
+	q.ultimo = NULL;
+	q.primero = NULL;
+	
+	//datos
+	string dato;
+	int op;
+	string x; //funcion de pop a la cola
+	
+	do{
+		menu();
+		cout<<"\t"; cin>>op;
+		
+		switch(op){
+			case 1:
+				cout<<"\n\t Color a Encolar: ";
+				cin>>dato;
+				encolar(q,dato);
+				cout<<"\n\t Color: "<<dato<<" encolado...\n";
+			break;
+			
+			case 2:
+				x = desencolar(q);
+				cout<<"\n\t Color: "<<x<<" desencolado...\n";
+			break;
+			
+			case 3:
+				cout<<"\n\t Mostrar Cola: \n";
+				//saber que no este null
+				if(q.ultimo != NULL) muestracola(q);
+				else cout<<"\n\t Cola Vacia...\n";
+			break;
+			
+			case 4:
+				vaciarcola(q);
+				cout<<"\n\t Colita se Vacio...\n";
+			break;
+			
+		}
+	}while(op != 5);
+	
+	return 0;
+}
